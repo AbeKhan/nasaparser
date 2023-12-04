1ee44d6f042211371afe6c73ae3e31bc43169413
AbeKhan <128449118+AbeKhan@users.noreply.github.com>
2023-12-04 15:33:15 -0600

    first commit

---
+++ src/main/java/edu/francistuttle/App.java
@@ -0,0 +1,14 @@
+package edu.francistuttle;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        RomeEx2 nasa = new RomeEx2();
+       nasa.parse();
+    }
+}

e03ffb8afe543147b758d4d1d0b88216fddc3497
AbeKhan <128449118+AbeKhan@users.noreply.github.com>
2023-12-06 15:04:34 -0600

    finish, i think

--- src/main/java/edu/francistuttle/App.java
+++ src/main/java/edu/francistuttle/App.java
@@ -8,7 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        RomeEx2 nasa = new RomeEx2();
-        //nasa.parse();
+    
     }
 }
--- src/main/java/edu/francistuttle/RomeEx2.java
+++ src/main/java/edu/francistuttle/RomeEx2.java
@@ -15,39 +15,28 @@ import java.util.List;
 public class RomeEx2 {
     public static void main(String[] args) {
         try {/* ww w . j a va 2 s . c om */
-            // String urlString = "http://feeds.reuters.com/reuters/topNews";
-            // URL feedUrl = new URL(urlString);
+            String urlString = "https://www.nasa.gov/news-release/feed/";
+            URL feedUrl = new URL(urlString);
 
-            File nasaFile = new File("C:/Users/ak1048135/Downloads/Abe's git folder/nasaparser/nasa.xml");
+           // File nasaFile = new File("C:/Users/ak1048135/Downloads/Abe's git folder/nasaparser/nasa.xml");
 
             SyndFeedInput input = new SyndFeedInput();
-            SyndFeed feed = input.build(new XmlReader(nasaFile));
+            //SyndFeed feed = input.build(new XmlReader(nasaFile));
+            
+            SyndFeed feed = input.build(new XmlReader(feedUrl));
 
             System.out.println("Feed Title: " + feed.getTitle());
+            System.out.println("Link: " + feed.getLink());
+            System.out.println("Description: " + feed.getDescription());
+            System.out.println("");
+
             // Get the entry items...
             for (SyndEntry entry : (List<SyndEntry>) feed.getEntries()) {
                 System.out.println("Title: " + entry.getTitle());
-                System.out.println("Unique Identifier: " + entry.getUri());
-                System.out.println("Updated Date: "
-                        + entry.getUpdatedDate());
-
-                // Get the Links
-                for (SyndLinkImpl link : (List<SyndLinkImpl>) entry
-                        .getLinks()) {
-                    System.out.println("Link: " + link.getHref());
-                }
-
-                // Get the Contents
-                for (SyndContentImpl content : (List<SyndContentImpl>) entry
-                        .getContents()) {
-                    System.out.println("Content: " + content.getValue());
-                }
-
-                // Get the Categories
-                for (SyndCategoryImpl category : (List<SyndCategoryImpl>) entry
-                        .getCategories()) {
-                    System.out.println("Category: " + category.getName());
-                }
+                //System.out.println("Description: " + entry.getDescription().getValue());
+                System.out.println("Link: " + entry.getLink());
+                System.out.println("");
+                
             }
         } catch (Exception ex) {
             System.out.println("Error: " + ex.getMessage());
